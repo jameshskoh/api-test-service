a1c14874ec90fd40c05d06d5b4573a0d8131dd45
James Koh <huisoonkoh@gmail.com>
2024-03-15 00:57:21 +0800

    feat(NO_TICKET) Push trivial content

--- src/main/java/com/jameshskoh/apitestservice/Main.java
+++ src/main/java/com/jameshskoh/apitestservice/Main.java
@@ -2,6 +2,6 @@ package com.jameshskoh.apitestservice;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("LOL!");
+    System.out.println("LOL!LOL!");
   }
 }
