4e3afc7da1b1c25b7578e5766a2d4a0bc977d9bc
James Koh <huisoonkoh@gmail.com>
2024-03-14 12:17:42 +0800

    feat(NO_TICKET) Initial commit

---
+++ src/main/java/com/jameshskoh/apitestservice/ApiTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jameshskoh.apitestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jameshskoh/apitestservice/ApiTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameshskoh.apitestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
