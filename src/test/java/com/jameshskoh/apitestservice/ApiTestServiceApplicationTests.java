e06f8880f6b1dd52705acd088a434294caf493d7
James Koh <huisoonkoh@gmail.com>
2024-03-21 00:11:00 +0800

    feat(NO_TICKET) Simplify sample test case

--- src/test/java/com/jameshskoh/apitestservice/ApiTestServiceApplicationTests.java
+++ src/test/java/com/jameshskoh/apitestservice/ApiTestServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package com.jameshskoh.apitestservice;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiTestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void whenRun_shouldAlwaysPass() {
+    var someValue = 1;
 
+    Assertions.assertThat(someValue).isBetween(0, 2);
+  }
 }
