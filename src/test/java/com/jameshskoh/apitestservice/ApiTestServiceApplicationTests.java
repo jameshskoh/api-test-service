8f68c7bc6e8792c94bd5117906e5dfe6235e047f
James Koh <huisoonkoh@gmail.com>
2024-03-23 00:07:27 +0800

    feat(NO_TICKET) Add failing tests

--- src/test/java/com/jameshskoh/apitestservice/ApiTestServiceApplicationTests.java
+++ src/test/java/com/jameshskoh/apitestservice/ApiTestServiceApplicationTests.java
@@ -21,10 +21,10 @@ class ApiTestServiceApplicationTests {
     Assertions.assertThat(someValue).isBetween(0, 2);
   }
 
-  //  @Test
-  //  void failingTest() {
-  //    var someValue = 3;
-  //
-  //    Assertions.assertThat(someValue).isBetween(0, 2);
-  //  }
+  @Test
+  void failingTest() {
+    var someValue = 3;
+
+    Assertions.assertThat(someValue).isBetween(0, 2);
+  }
 }
